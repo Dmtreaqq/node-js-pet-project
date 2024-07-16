dc3d93a79705e0f5ffaccc08d104ead1ad28f3de
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-07-16 17:05:31 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+
+  const str = "Dima"
+
+  res.send(`Hello ${str}`);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
